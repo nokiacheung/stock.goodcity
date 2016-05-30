9956d2426fd1ec6ef6e353eddc93ce95d6cf3ff4
swatijadhav <swati.jadhav@kiprosh.com>
2016-05-30 18:17:09 +0530

    component to designate item to an order

---
+++ app/components/designate-form.js
@@ -0,0 +1,15 @@
+import Ember from "ember";
+
+export default Ember.Component.extend({
+
+  actions: {
+    displayDesignateForm() {
+      this.set("displayUserPrompt", true);
+    },
+
+    designateItem() {
+
+    }
+  }
+
+});
--- app/routes/orders/items.js
+++ app/routes/orders/items.js
@@ -4,4 +4,9 @@ export default AuthorizeRoute.extend({
   model(params) {
     return this.store.findRecord("designation", params.order_id);
   },
+
+  setupController(controller, model){
+    this._super(controller, model);
+    controller.set('searchText', "");
+  }
 });
