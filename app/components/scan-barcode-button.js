cd7c6e40326222a89f49d20b739883eb6e38db57
swatijadhav <swati.jadhav@kiprosh.com>
2016-06-14 16:32:29 +0530

    GCW-1045 allow adding of items to order by barcode

---
+++ app/components/scan-barcode-button.js
@@ -0,0 +1,33 @@
+import Ember from "ember";
+import config from '../config/environment';
+
+export default Ember.Component.extend({
+
+  messageBox: Ember.inject.service(),
+  order: null,
+
+  actions: {
+
+    scanBarcode() {
+
+      if (!config.cordova.enabled) {
+        this.get('router').transitionTo("orders.items", this.get("order"), {queryParams: { searchText: null }});
+      } else {
+
+        var onSuccess = res => {
+          if (!res.cancelled) {
+            console.log(res.text);
+            this.get('router').transitionTo("orders.items", this.get("order"), {queryParams: { searchText: res.text } });
+          }
+        };
+        var onError = error => this.get("messageBox").alert("Scanning failed: " + error);
+        var options = {"formats": "CODE_128"};
+
+        window.cordova.plugins.barcodeScanner.scan(onSuccess, onError, options);
+
+      }
+
+    },
+  }
+
+});
