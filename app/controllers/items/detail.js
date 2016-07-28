fd167188d539da367db2000f38424a466f1c61b5
swatijadhav <swati.jadhav@kiprosh.com>
2016-07-28 14:15:23 +0530

    GCW-1131 gradient-effect for item-image to display back link

--- app/controllers/items/detail.js
+++ app/controllers/items/detail.js
@@ -2,4 +2,9 @@ import Ember from "ember";
 
 export default Ember.Controller.extend({
   item: Ember.computed.alias("model"),
+
+  previewImageBgCss: Ember.computed("model.imageUrl", function() {
+    return new Ember.Handlebars.SafeString("background: linear-gradient(180deg, rgba(0, 0, 0, 1) -50%, transparent), url(" + this.get("model.imageUrl") + ") center;");
+  }),
+
 });
