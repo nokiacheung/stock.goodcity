a0b50dffc235ca4e978f53f77cec94d2cec08b2b
swatijadhav <swati.jadhav@kiprosh.com>
2016-08-02 17:47:45 +0530

    GCW-1177 fixed back link at order-details page

--- app/routes/orders/detail.js
+++ app/routes/orders/detail.js
@@ -3,21 +3,30 @@ import Ember from 'ember';
 
 export default getOrderRoute.extend({
 
-  backLinkPath: Ember.computed.localStorage(),
+  orderBackLinkPath: Ember.computed.localStorage(),
 
   beforeModel() {
     var previousRoutes = this.router.router.currentHandlerInfos;
     var previousRoute = previousRoutes && previousRoutes.pop();
     var path = "orders.index";
-    if(previousRoute && previousRoute.name !== "orders.items") {
-      path = previousRoute.name === "items.search_order" ? "items" : previousRoute.name;
+
+    if(previousRoute) {
+      var routeName = previousRoute.name;
+      if(routeName.indexOf("orders")) {
+        path = routeName === "items.search_order" ? "items" : routeName;
+      } else if(routeName === path) {
+        path = path;
+      } else if(routeName.indexOf("orders") === 0) {
+        path = this.get("orderBackLinkPath") || path;
+      }
     }
-    this.set("backLinkPath", path);
+
+    this.set("orderBackLinkPath", path);
   },
 
   setupController(controller, model){
     this._super(controller, model);
     controller.set('displayAllItems', model.get('items.length') <= 3);
-    controller.set('backLinkPath', this.get('backLinkPath'));
+    controller.set('backLinkPath', this.get('orderBackLinkPath'));
   }
 });
