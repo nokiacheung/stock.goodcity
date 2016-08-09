18cbbbda37bf433020339a4ea9da362a88b4648e
swatijadhav <swati.jadhav@kiprosh.com>
2016-08-09 14:24:44 +0530

    hide designate option when all items dispatched from set

--- app/controllers/items/detail.js
+++ app/controllers/items/detail.js
@@ -4,4 +4,13 @@ export default Ember.Controller.extend({
   item: Ember.computed.alias("model"),
   backLinkPath: "",
 
+  allDispatched: Ember.computed("item.{isDispatched,isSet,setItem.items.@each.isDispatched}", function() {
+    if(this.get("item.isSet")) {
+      var allItems = this.get("item.setItem.items");
+      return allItems.length === allItems.filterBy("isDispatched").length;
+    } else {
+      return this.get("item.isDispatched");
+    }
+  }),
+
 });
