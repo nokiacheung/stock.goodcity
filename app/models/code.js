8403cb105d765d03e836a390b856fef3106c112b
swatijadhav <swati.jadhav@kiprosh.com>
2016-08-23 18:31:16 +0530

    GCW-1190 UI changes for the new-item form

--- app/models/code.js
+++ app/models/code.js
@@ -5,6 +5,34 @@ import { belongsTo } from 'ember-data/relationships';
 export default Model.extend({
 
   name:             attr('string'),
+  code:             attr('string'),
   visibleInSelects: attr('boolean', { defaultValue: false }),
   location:         belongsTo('location', { async: false }),
+
+  defaultChildPackages: attr('string'),
+  otherChildPackages:   attr('string'),
+
+  defaultChildPackagesList: function() {
+    return this._getPackages(this, this.get("defaultChildPackages"));
+  },
+
+  otherChildPackagesList: function() {
+    return this._getPackages(this, this.get("otherChildPackages"));
+  },
+
+  allChildPackagesList: function() {
+    return this.defaultChildPackagesList().concat(this.otherChildPackagesList());
+  },
+
+  _getPackages: function(model, packageNames){
+    var array = (packageNames || "").split(',');
+    var packages = [];
+    var allPackageTypes = model.store.peekAll("code");
+    array.forEach(function(type) {
+      allPackageTypes.filter(function (pkg) {
+        return pkg.get("code") === type ? packages.push(pkg) : "";
+      });
+    });
+    return packages;
+  }
 });
