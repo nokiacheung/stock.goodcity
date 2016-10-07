2b4797cc99c907bf8fafd8525376ba9603b1f11d
namrataukirde <namrata.ukirde@kiprosh.com>
2016-10-07 13:49:00 +0530

    GCW-1295-update-app-to-ember-2.8

--- app/helpers/apply-line-break.js
+++ app/helpers/apply-line-break.js
@@ -3,5 +3,5 @@ import Ember from "ember";
 export default Ember.Helper.helper(function(value) {
   value = Ember.Handlebars.Utils.escapeExpression(value || "");
   value = value.replace(/(\r\n|\n|\r)/gm, '<br>');
-  return new Ember.Handlebars.SafeString(value);
+  return new Ember.String.htmlSafe(value);
 });
