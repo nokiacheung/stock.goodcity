e9a56ff90b25daa954e1c7b48f59289c0fd7adbe
Arthi Jaganathan <jaganathan.arthi@gmail.com>
2016-11-10 10:56:03 +0800

    GCW-1335 updated logic for android app version code

--- cordova/scripts/update_build_number.py
+++ cordova/scripts/update_build_number.py
@@ -11,15 +11,22 @@
 # iTunesConnect allows uploading multiple builds for a given MAJOR.MINOR.PATCH.
 # Not specifying CFBundleVersion sets the build number to the same as version number
 #
-# For android builds, versionCode is set to CircleCI build number.
+# For android builds, versionCode is set to CircleCI build number + 1000.
 # Cordova build will suffix 8 or 9 based on SDK version supported.
-# For ex, a Circle CI build 1800 will map to version 18008 on Google Play.
+# For ex, a Circle CI build 590 will map to version 15908 on Google Play.
+# At the time of implementing this script, version on Google Play was 12008
+# and the circle ci build was 590
+# Hence the need to bump the circleci build number so that Google Play
+# version is greater than 12008. The choice of 1000 as seed is arbitrary.
+#
 # For iOS builds, CFBundleVersion is set to the Circle CI build niumber.
 # There is a one to one mapping to the build number on iTunesConnect
 
 import os
 import sys
 from xml.etree import ElementTree
+
+ANDROID_BUILD_VERSION_SEED = 1000
 
 def get_circleci_build():
     build_number = os.getenv('CIRCLE_BUILD_NUM')
@@ -32,7 +39,7 @@ def get_ios_build_no():
     return get_circleci_build()
 
 def get_android_build_no():
-    return get_circleci_build()
+    return str(ANDROID_BUILD_VERSION_SEED + int(get_circleci_build()))
 
 cordova_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
 config_xml_path = os.path.join(cordova_path, 'config.xml')
