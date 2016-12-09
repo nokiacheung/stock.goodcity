c47622b0136bc48696d5d3cbf3099afac3869953
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2016-12-09 10:55:18 +0530

    Recently_used locations and designations loaded at home page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,10 @@
 import SessionRoute from './session';
 
-export default SessionRoute.extend();
+export default SessionRoute.extend({
+  model() {
+    var recentlyUsedDesignations = this.get('store').query('designation', { recently_used: true });
+    var recentlyUsedLocations = this.get('store').query('location', { recently_used: true });
+    this.get('store').pushPayload(recentlyUsedDesignations);
+    this.get('store').pushPayload(recentlyUsedLocations);
+  }
+});
