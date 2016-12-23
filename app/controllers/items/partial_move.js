0c2ccb74ea0c1d5c1707347cd192b82cd697622e
namrataukirde <namrata.ukirde@kiprosh.com>
2016-12-23 20:14:00 +0530

    change ui for partial move

--- app/controllers/items/partial_move.js
+++ app/controllers/items/partial_move.js
@@ -2,15 +2,35 @@ import Ember from "ember";
 
 export default Ember.Controller.extend({
   item: null,
+  // packagesLoacationQty: null,
 
   totalInHandItems: Ember.computed('item.quantity', function() {
     return this.get('item.quantity');
   }),
 
+  packagesLoacationQty: Ember.computed.localStorage(),
+  totalQty: Ember.computed.localStorage(),
+
   actions: {
     move_partial_qty(item) {
-      var partial_qty = parseInt(Ember.$(`#${item.id}`)[0].value);
-      this.transitionToRoute('items.search_location', item.id, {queryParams: {partial_qty: partial_qty}});
+      var totalQty = 0;
+      var allPackagesLocations = item.get('packages_locations');
+      var elementIds  = allPackagesLocations.getEach('id');
+      var packagesLoacationQty = [];
+      var record = {};
+      elementIds.forEach(packages_location_id => {
+        var value = parseInt(Ember.$(`#${packages_location_id}`)[0].value);
+        var packages_location = this.get('store').peekRecord('packages_location', packages_location_id);
+        record["packages_location_id"] = packages_location_id;
+        record["package_id"] = packages_location.get('package_id');
+        record["new_qty"] = value;
+        totalQty += value;
+        packagesLoacationQty.push(record);
+        record = {};
+      });
+      this.set("packagesLoacationQty", packagesLoacationQty);
+      this.set("totalQty", totalQty);
+      this.transitionToRoute('items.search_location', item.id);
     }
   }
 });
