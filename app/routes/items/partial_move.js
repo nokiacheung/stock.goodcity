43e883bb0269d2d64828fc951de717bb80d282c4
namrataukirde <namrata.ukirde@kiprosh.com>
2017-01-04 17:14:00 +0530

    partial move for set

--- app/routes/items/partial_move.js
+++ app/routes/items/partial_move.js
@@ -4,5 +4,13 @@ export default AuthorizeRoute.extend({
   model(params){
      var item = this.store.peekRecord("item", params.item_id);
      return item || this.store.findRecord('item', params.item_id);
+  },
+
+  afterModel(model){
+    if(model.get('isSet')){
+      model.get('setItem.items').forEach(record => {
+        this.store.findRecord('item', record.get('id'));
+      });
+    }
   }
 });
