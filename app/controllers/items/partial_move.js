dcd0983987b11613cc5dafa5fd8284c869742c2d
namrataukirde <namrata.ukirde@kiprosh.com>
2017-01-05 19:11:51 +0530

    changes to api request for partial move of set

--- app/controllers/items/partial_move.js
+++ app/controllers/items/partial_move.js
@@ -11,6 +11,7 @@ export default Ember.Controller.extend({
   }),
 
   packagesLoacationQty: Ember.computed.localStorage(),
+  setPackagesLoacationQty: Ember.computed.localStorage(),
   totalQty: Ember.computed.localStorage(),
   originalQty: null,
 
@@ -37,6 +38,33 @@ export default Ember.Controller.extend({
         this.set("isZeroQuantity", true);
       }else{
         this.transitionToRoute('items.search_location', item.id, { queryParams: { isPartialMove: true }});
+      }
+    },
+
+    move_partial_qty_for_set(item) {
+      var totalQty = 0;
+      var record1 = {};
+      var packagesLoacationQty = [];
+      if(item.get('isSet')){
+        item.get('setItem.items').forEach(record => {
+          record.get('packages_locations').getEach('id').forEach(packages_location_id => {
+            var value = parseInt(Ember.$(`#${packages_location_id}`)[0].value);
+            var packages_location = this.get('store').peekRecord('packages_location', packages_location_id);
+            record1['packages_location_id'] = packages_location_id;
+            record1['package_id'] = packages_location.get('package_id');
+            record1["new_qty"] = value;
+            totalQty += value;
+            packagesLoacationQty.push(record1);
+            record1 = {};
+          });
+          this.set('setPackagesLoacationQty', packagesLoacationQty);
+          this.set("totalQty", totalQty);
+          if(this.get('totalQty') === 0){
+            this.set("isZeroQuantity", true);
+          } else {
+            this.transitionToRoute('items.search_location', item.id, { queryParams: { isPartialMove: true , isSet: true}});
+          }
+        });
       }
     }
   }
