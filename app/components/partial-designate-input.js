cda9fe16776678a0baa5a2fd2cb35b366d56a70d
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-01-24 12:56:32 +0530

    Cannot read property send: fixed

--- app/components/partial-designate-input.js
+++ app/components/partial-designate-input.js
@@ -3,10 +3,8 @@ import Ember from "ember";
 export default Ember.TextField.extend({
   item: null,
   value: "",
-  tagName: "input",
-  type:    "number",
   maxlength: "5",
-  attributeBindings: [ "name", "id", "value", 'placeholder', 'min', 'max'],
+  attributeBindings: [ "name", "id", "value", 'placeholder'],
   partial_qty_value: Ember.computed.alias('value'),
   designateFullSet: Ember.computed.localStorage(),
 
@@ -33,8 +31,21 @@ export default Ember.TextField.extend({
     Ember.$(this.element).css("background-color", "#002352");
   },
 
+  focusOut() {
+    var regex = /^\d+$/;
+    var input_value = this.get('value');
+    if(input_value <= 0 || (this.get('item.isSet') && input_value > this.get('minSetQty')) || input_value > this.get('item.quantity') || !(regex.test(input_value))) {
+      Ember.$(this.element).css("border", "1px solid #fddbdc");
+      Ember.$('#partial_designate')[0].disabled = true;
+      this.$().focus();
+      return false;
+    }
+  },
+
   focusTrigger: Ember.observer('value', function() {
-    if(this.get('value') <= 0 || (this.get('item.isSet') && this.get('value') > this.get('minSetQty')) || this.get('value') > this.get('item.quantity')) {
+    var regex = /^\d+$/;
+    var input_value = this.get('value');
+    if(input_value <= 0 || (this.get('item.isSet') && input_value > this.get('minSetQty')) || input_value > this.get('item.quantity') || !(regex.test(input_value))) {
       Ember.$(this.element).css("border", "1px solid #fddbdc");
       Ember.$('#partial_designate')[0].disabled = true;
       this.$().focus();
