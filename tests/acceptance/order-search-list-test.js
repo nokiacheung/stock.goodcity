4be10d462f0080cb77857d8a9417770df0d4229f
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-01-24 18:25:53 +0530

    Order-search list test case added

--- tests/acceptance/order-search-list-test.js
+++ tests/acceptance/order-search-list-test.js
@@ -8,7 +8,7 @@ import FactoryGuy from 'ember-data-factory-guy';
 import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
 import { mockFindAll } from 'ember-data-factory-guy';
 
-var App, designation, item, orders_package;
+var App, designation, item, item1, orders_package, orders_package1;
 
 module('Order search list', {
   beforeEach: function() {
@@ -16,7 +16,9 @@ module('Order search list', {
     TestHelper.setup();
     designation = FactoryGuy.make("designation", { state: "closed" });
     item = FactoryGuy.make("item", { state: "submitted" });
+    item1 = FactoryGuy.make("item", { state: "submitted" });
     orders_package = FactoryGuy.make("orders_package", { state: "designated", item: item, designation: designation });
+    orders_package1 = FactoryGuy.make("orders_package", { state: "dispatched", item: item1, designation: designation });
   },
   afterEach: function() {
     Ember.run(function() { TestHelper.teardown(); });
@@ -24,17 +26,32 @@ module('Order search list', {
   }
 });
 
-test("display order with count of designated and dispatched orders_packages", function(assert) {
+test("Order has designated OrdersPackage", function(assert) {
   assert.expect(1);
 
   visit("/orders/");
 
   mockFindAll('designation').returns({ json: {designations: [designation.toJSON({includeId: true})], items: [item.toJSON({includeId: true})], orders_packages: [orders_package.toJSON({includeId: true})], meta: {search: designation.get('code').toString()}}});
-  mockFindAll('orders_package').returns({ json: {orders_packages: [orders_package.toJSON({includeId: true})]}});
+    mockFindAll('orders_package').returns({ json: {orders_packages: [orders_package.toJSON({includeId: true})]}});
 
   fillIn('#searchText', designation.get("code"));
 
   andThen(function(){
     assert.equal(parseInt($('ul.list li:first div.small-4.columns').text().trim().replace(/ +/g, "")), 1);
+  });
+});
+
+test("Order has dispatched OrdersPackage", function(assert) {
+  assert.expect(1);
+
+  visit("/orders/");
+
+  mockFindAll('designation').returns({ json: {designations: [designation.toJSON({includeId: true})], items: [item1.toJSON({includeId: true})], orders_packages: [orders_package1.toJSON({includeId: true})], meta: {search: designation.get('code').toString()}}});
+    mockFindAll('orders_package').returns({ json: {orders_packages: [orders_package1.toJSON({includeId: true})]}});
+
+  fillIn('#searchText', designation.get("code"));
+
+  andThen(function(){
+    assert.equal(parseInt($('ul.list li:first div.small-4.columns').text().trim().replace(/ +/g, "").slice(-1)), 1);
   });
 });
