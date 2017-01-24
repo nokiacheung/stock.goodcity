7f10c318550e1ae6d7d49c012fe2f91b0ff3a71f
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-01-24 18:14:58 +0530

    Failing test fixed

--- tests/acceptance/order-search-list-test.js
+++ tests/acceptance/order-search-list-test.js
@@ -6,6 +6,7 @@ import '../factories/designation';
 import '../factories/item';
 import FactoryGuy from 'ember-data-factory-guy';
 import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
+import { mockFindAll } from 'ember-data-factory-guy';
 
 var App, designation, item, orders_package;
 
@@ -24,13 +25,16 @@ module('Order search list', {
 });
 
 test("display order with count of designated and dispatched orders_packages", function(assert) {
-  var data = {"designations":[{"status":"Active","code": "L24401","id":1,"item_ids":[1]}], "items":[{"id":1,"inventory_number":"C4234","orders_package_ids":[1255,1256,1271]}],"code":[{"id":143,"name":"Assorted/Mixed Box of Eyeglasses","code":"MEW","other_child_packages":null,"default_child_packages":"MEW","other_terms":null,"visible_in_selects":true,"location_id":null}],"locations":[{"id":7,"building":"24","area":"D","stockit_id":7}],"donor_conditions":[{"id":3,"name":"Heavily Used"}],"orders_packages":[{"id":101,"package_id":1,"order_id":1,"state":"designated","quantity":1,"designation_id":1,"item_id":1}
-      ]};
   assert.expect(1);
-  $.mockjax({url:"/api/v1/designation*", status:200, responseText: data});
+
   visit("/orders/");
+
+  mockFindAll('designation').returns({ json: {designations: [designation.toJSON({includeId: true})], items: [item.toJSON({includeId: true})], orders_packages: [orders_package.toJSON({includeId: true})], meta: {search: designation.get('code').toString()}}});
+  mockFindAll('orders_package').returns({ json: {orders_packages: [orders_package.toJSON({includeId: true})]}});
+
   fillIn('#searchText', designation.get("code"));
+
   andThen(function(){
-    assert.equal($('ul.list li:first div.small-4.columns').text().trim(), 1);
+    assert.equal(parseInt($('ul.list li:first div.small-4.columns').text().trim().replace(/ +/g, "")), 1);
   });
 });
