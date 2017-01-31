c50a01b03417d7b59bcc0c6cd553309fdfae9df6
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-01-31 14:59:51 +0530

    CircleCi build fixed

--- tests/acceptance/item-available-option-test.js
+++ tests/acceptance/item-available-option-test.js
@@ -4,15 +4,18 @@ import startApp from '../helpers/start-app';
 import '../factories/orders_package';
 import '../factories/designation';
 import '../factories/item';
+import '../factories/location';
 import FactoryGuy from 'ember-data-factory-guy';
 import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
+import { mockFindAll } from 'ember-data-factory-guy';
 
 var App, pkg, pkg1, pkg2, order1, order2, orders_pkg1, orders_pkg2;
 
-module('Acceptance: Item Detail', {
+module('Acceptance: Item Available actions', {
   beforeEach: function() {
     App = startApp({}, 2);
     TestHelper.setup();
+    var location = FactoryGuy.make("location");
     order1 = FactoryGuy.make("designation", { id: 100, state: "submitted" });
     order2 = FactoryGuy.make("designation", { id: 101, state: "submitted" });
     pkg = FactoryGuy.make("item", { id: 50, state: "submitted" , quantity: 10});
@@ -20,6 +23,8 @@ module('Acceptance: Item Detail', {
     pkg2 = FactoryGuy.make("item", { id: 52, state: "submitted" , designation: order2, quantity: 0});
     orders_pkg1 = FactoryGuy.make("orders_package", { id: 500, state: "designated", quantity: 6, item: pkg1, designation: order1 });
     orders_pkg2 = FactoryGuy.make("orders_package", { id: 501, state: "dispatched", quantity: 10, item: pkg2, designation: order2 });
+    mockFindAll('designation').returns({json: {designations: [order1.toJSON({includeId: true})]}});
+    mockFindAll('location').returns({json: {locations: [location.toJSON({includeId: true})]}});
   },
   afterEach: function() {
     Ember.run(function() { TestHelper.teardown(); });
@@ -29,63 +34,42 @@ module('Acceptance: Item Detail', {
 
 test("Available actions for non-designated item", function(assert) {
   assert.expect(2);
-  $.mockjax({url: '/api/v1/stockit_item*', type: 'GET', status: 200,responseText: {
-      items: [pkg.toJSON({includeId: true})]
-      }
-  });
-  visit("/items/"+ pkg.id);
+  visit("/items");
+  fillIn("#searchText", pkg.get("inventoryNumber"));
+  mockFindAll('item').returns({ json: {items: [pkg.toJSON({includeId: true})], orders_packages: [orders_pkg1.toJSON({includeId: true})], designations:[order2.toJSON({includeId: true})], meta: {search: pkg.get('inventoryNumber').toString()}}});
   andThen(function() {
-    assert.equal(currentPath(), "items.detail");
-    click($('.item_options_component div.options-link-open'));
+    assert.equal(currentPath(), "items.index");
+    click($('div.options-link-open'));
     //dispatch disabled(all other options are enabled)
     assert.equal($('div.receive-item-options div:eq(4)')[0].className, 'disabled');
   });
 });
 
 test("Available actions for partially-designated item(qty > 0)", function(assert) {
-  $.mockjax({url: '/api/v1/stockit_item*', type: 'GET', status: 200,responseText: {
-      items: [pkg1.toJSON({includeId: true})]
-      }
-  });
   assert.expect(2);
-  visit("/items/"+ pkg1.id);
+   visit("/items");
+  fillIn("#searchText", pkg1.get("inventoryNumber"));
+  mockFindAll('item').returns({ json: {items: [pkg1.toJSON({includeId: true})], orders_packages: [orders_pkg1.toJSON({includeId: true})], designations:[order2.toJSON({includeId: true})], meta: {search: pkg1.get('inventoryNumber').toString()}}});
   andThen(function() {
-    assert.equal(currentPath(), "items.detail");
-    click($('.item_options_component div.options-link-open'));
+    assert.equal(currentPath(), "items.index");
+    click($('div.options-link-open'));
     //All actions are enabled including dispatch
     assert.equal($('div.receive-item-options div.disabled')[0], undefined);
   });
 });
 
 test("Available actions for fully-designated item(qty = 0)", function(assert) {
-  $.mockjax({url: '/api/v1/stockit_item*', type: 'GET', status: 200,responseText: {
-      items: [pkg2.toJSON({includeId: true})]
-      }
-  });
   assert.expect(3);
-  visit("/items/"+ pkg2.id);
+   visit("/items");
+  fillIn("#searchText", pkg2.get("inventoryNumber"));
+  mockFindAll('item').returns({ json: {items: [pkg2.toJSON({includeId: true})], orders_packages: [orders_pkg2.toJSON({includeId: true})], designations:[order2.toJSON({includeId: true})], meta: {search: pkg2.get('inventoryNumber').toString()}}});
   andThen(function() {
-    assert.equal(currentPath(), "items.detail");
-    click($('.item_options_component div.options-link-open'));
+    assert.equal(currentPath(), "items.index");
+    click($('div.options-link-open'));
     //Move is disabled as qty = 0
     assert.equal($('div.receive-item-options div.disabled:first').text().trim(), "Move");
     //designate is disabled
     assert.equal($('div.receive-item-options div.disabled:last').text().trim(), "designate");
-  });
-});
-
-test("Redirects to history page", function(assert) {
-  assert.expect(1);
-  $.mockjax({url: '/api/v1/stockit_item*', type: 'GET', status: 200,responseText: {
-      items: [pkg2.toJSON({includeId: true})]
-      }
-  });
-  visit("/items/"+ pkg2.id);
-  andThen(function() {
-    click($('ul.small-block-grid-3 li:eq(2)'));
-  });
-  andThen(function() {
-    assert.equal(currentPath(), "items.history");
   });
 });
 
