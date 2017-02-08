90c38b6c6c99f5c4899ff5c43cf6527da2e5e676
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-02-08 11:38:16 +0530

    config removed for comoponent testing

--- app/components/partial-designate-input.js
+++ app/components/partial-designate-input.js
@@ -1,29 +1,25 @@
 import Ember from "ember";
-import config from '../config/environment';
 
 export default Ember.TextField.extend({
   item: null,
   value: "",
   tagName: "input",
   maxlength: "5",
-  ENV: config,
   attributeBindings: [ "name", "id", "value", 'placeholder'],
   partial_qty_value: Ember.computed.alias('value'),
   designateFullSet: Ember.computed.localStorage(),
 
   minSetQty: Ember.computed('item.setItem.items', function() {
-    if(this.get("ENV.environment") !== "test") {
-      if(this.get('item.isSet') && this.get('designateFullSet')) {
-        var setItems = this.get('item.setItem.items');
-        var minQty = setItems.canonicalState[0]._data.quantity;
-        setItems.canonicalState.forEach(record =>{
-          var qty = record._data.quantity;
-          if(qty < minQty) {
-            minQty = qty;
-          }
-        });
-        return minQty;
-      }
+    if(this.get('item.isSet') && this.get('designateFullSet')) {
+      var setItems = this.get('item.setItem.items');
+      var minQty = setItems.canonicalState[0]._data.quantity;
+      setItems.canonicalState.forEach(record =>{
+        var qty = record._data.quantity;
+        if(qty < minQty) {
+          minQty = qty;
+        }
+      });
+      return minQty;
     }
   }),
 
@@ -36,32 +32,28 @@ export default Ember.TextField.extend({
   },
 
   focusOut() {
-    if(this.get("ENV.environment") !== "test") {
-      var regex = /^\d+$/;
-      var input_value = this.get('value');
-      if(input_value <= 0 || (this.get('item.isSet') && input_value > this.get('minSetQty')) || input_value > this.get('item.quantity') || !(regex.test(input_value))) {
-        Ember.$(this.element).css("border", "1px solid #fddbdc");
-        Ember.$('#partial_designate')[0].disabled = true;
-        this.$().focus();
-        return false;
-      }
+    var regex = /^\d+$/;
+    var input_value = this.get('value');
+    if(input_value <= 0 || (this.get('item.isSet') && input_value > this.get('minSetQty')) || input_value > this.get('item.quantity') || !(regex.test(input_value))) {
+      Ember.$(this.element).css("border", "1px solid #fddbdc");
+      Ember.$('#partial_designate')[0].disabled = true;
+      this.$().focus();
+      return false;
     }
   },
 
   focusTrigger: Ember.observer('value', function() {
-    if(this.get("ENV.environment") !== "test") {
-      var regex = /^\d+$/;
-      var input_value = this.get('value');
-      if(input_value <= 0 || (this.get('item.isSet') && input_value > this.get('minSetQty')) || input_value > this.get('item.quantity') || !(regex.test(input_value))) {
-        Ember.$(this.element).css("border", "1px solid #fddbdc");
-        Ember.$('#partial_designate')[0].disabled = true;
-        this.$().focus();
-        return false;
-      } else {
-        Ember.$(this.element).css("border", "1px solid #8091a9");
-        Ember.$('#partial_designate')[0].disabled = false;
-        return true;
-      }
+    var regex = /^\d+$/;
+    var input_value = this.get('value');
+    if(input_value <= 0 || (this.get('item.isSet') && input_value > this.get('minSetQty')) || input_value > this.get('item.quantity') || !(regex.test(input_value))) {
+      Ember.$(this.element).css("border", "1px solid #fddbdc");
+      Ember.$('#partial_designate')[0].disabled = true;
+      this.$().focus();
+      return false;
+    } else {
+      Ember.$(this.element).css("border", "1px solid #8091a9");
+      Ember.$('#partial_designate')[0].disabled = false;
+      return true;
     }
   }),
 });
--- app/components/partial-undesignate-input.js
+++ app/components/partial-undesignate-input.js
@@ -1,11 +1,9 @@
 import Ember from "ember";
-import config from '../config/environment';
 
 export default Ember.TextField.extend({
   value: "",
   tagName: "input",
   maxlength: "5",
-  ENV: config,
   attributeBindings: [ "name", "id", "value", 'placeholder'],
   partial_qty_value: Ember.computed.alias('value'),
   designationPackage: null,
@@ -29,39 +27,35 @@ export default Ember.TextField.extend({
   },
 
   focusOut() {
-    if(this.get("ENV.environment") !== "test") {
-      var regex = /^\d+$/;
-      var input_value = this.get('value');
-      if(input_value < 0 || parseInt(input_value) < 0 || parseInt(input_value) > this.get('designationPackage.quantity') || !(regex.test(input_value))) {
-        Ember.$(this.element).css("border", "1px solid #fddbdc");
-        Ember.$('#undesignateButton')[0].disabled = true;
-        this.$().focus();
-        return false;
-      }
+    var regex = /^\d+$/;
+    var input_value = this.get('value');
+    if(input_value < 0 || parseInt(input_value) < 0 || parseInt(input_value) > this.get('designationPackage.quantity') || !(regex.test(input_value))) {
+      Ember.$(this.element).css("border", "1px solid #fddbdc");
+      Ember.$('#undesignateButton')[0].disabled = true;
+      this.$().focus();
+      return false;
     }
   },
 
   focusTrigger: Ember.observer('value', function() {
-    if(this.get("ENV.environment") !== "test") {
-      var regex = /^\d+$/;
-      var input_value = this.get('value');
-      var id=this.get('designationPackage.packageId');
-      var orderspackages = this.get('store').peekAll('orders_package').filterBy('packageId', id).filterBy('quantity').filterBy("state", "designated");
-      var orderpackagesIds = orderspackages.getEach('id');
-      var total = 0;
-      orderpackagesIds.forEach(id => {
-        total += parseInt(Ember.$(`#${id}`)[0].value);
-      });
-      if(input_value < 0 || parseInt(input_value) < 0 || parseInt(input_value) > this.get('designationPackage.quantity') || !(regex.test(input_value))) {
-        Ember.$(this.element).css("border", "1px solid #fddbdc");
-        Ember.$('#undesignateButton')[0].disabled = true;
-        this.$().focus();
-        return false;
-      } else {
-        Ember.$(this.element).css("border", "1px solid #8091a9");
-        Ember.$('#undesignateButton')[0].disabled = false;
-        return true;
-      }
+    var regex = /^\d+$/;
+    var input_value = this.get('value');
+    var id=this.get('designationPackage.packageId');
+    var orderspackages = this.get('store').peekAll('orders_package').filterBy('packageId', id).filterBy('quantity').filterBy("state", "designated");
+    var orderpackagesIds = orderspackages.getEach('id');
+    var total = 0;
+    orderpackagesIds.forEach(id => {
+      total += parseInt(Ember.$(`#${id}`)[0].value);
+    });
+    if(input_value < 0 || parseInt(input_value) < 0 || parseInt(input_value) > this.get('designationPackage.quantity') || !(regex.test(input_value))) {
+      Ember.$(this.element).css("border", "1px solid #fddbdc");
+      Ember.$('#undesignateButton')[0].disabled = true;
+      this.$().focus();
+      return false;
+    } else {
+      Ember.$(this.element).css("border", "1px solid #8091a9");
+      Ember.$('#undesignateButton')[0].disabled = false;
+      return true;
     }
   }),
 });
