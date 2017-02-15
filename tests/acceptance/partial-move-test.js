0ec7a90df358f290e5989d1146645ce5355be27c
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-02-15 17:48:16 +0530

    Partial move test case added for dispatched qty

--- tests/acceptance/partial-move-test.js
+++ tests/acceptance/partial-move-test.js
@@ -81,3 +81,22 @@ test("Partially moving a Package", function(assert) {
   });
 });
 
+test("Move is enabled if full quantity is dispatched", function(assert) {
+  assert.expect(1);
+  var item1 = FactoryGuy.make("item", { state: "submitted", quantity: 5 });
+  var orders_package1 = FactoryGuy.make("orders_package", { state: "dispatched", quantity: 5 });
+  click($('.two-line-ellipsis'));
+  $.mockjax({url: '/api/v1/stockit_item*', type: 'GET', status: 200,responseText: {
+      items: [item1.toJSON({includeId: true})],
+      orders_packages: [orders_package1.toJSON({includeId: true})]
+      }
+  });
+  andThen(function() {
+    click($('.options-link-open'));
+    andThen(function() {
+      //move action is disabled
+      assert.equal($('.receive-item-options div:first.disabled')[0], undefined);
+    });
+  });
+});
+
