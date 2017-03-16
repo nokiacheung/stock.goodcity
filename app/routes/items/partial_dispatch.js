b79beef7dd5902eefdb0286549cd2268c76c64ec
namrataukirde <namrata.ukirde@kiprosh.com>
2017-03-16 18:01:04 +0530

    commented getOwner in route

--- app/routes/items/partial_dispatch.js
+++ app/routes/items/partial_dispatch.js
@@ -1,7 +1,9 @@
-import Ember from 'ember';
+// import Ember from 'ember';
 import AuthorizeRoute from './../authorize';
-const { getOwner } = Ember;
+// const { getOwner } = Ember;
 
 export default AuthorizeRoute.extend({
-
+  model(params){
+    return this.store.peekRecord("item", params.item_id) || this.store.findRecord('item', params.item_id);
+  },
 });
