e2d7728abd51c5d02a4f6b2075c1121774965f98
namrataukirde <namrata.ukirde@kiprosh.com>
2017-03-20 19:10:30 +0530

    show total total qty by skipping invalid input

--- app/components/partial-move-input.js
+++ app/components/partial-move-input.js
@@ -5,6 +5,7 @@ export default Ember.TextField.extend({
   item: null,
   value: "",
   total: 0,
+  recordToSkipId: null,
   env: config.APP.environment,
   tagName: "input",
   maxlength: "5",
@@ -51,6 +52,7 @@ export default Ember.TextField.extend({
 
     if(input_value < 0 || input_value > this.get('item.quantity') || !(input_value.trim()) || !(regex.test(input_value))) {
       Ember.$(this.element).closest('div').addClass("has-error");
+      this.set('disableMove', true);
       Ember.$('#partial_move')[0].disabled = true;
       this.$().focus();
       return false;
@@ -58,6 +60,7 @@ export default Ember.TextField.extend({
       Ember.$('#partial_move')[0].disabled = true;
     } else {
       Ember.$(this.element).closest('div').removeClass("has-error");
+      this.set('disableMove', false);
       Ember.$('#partial_move')[0].disabled = false;
       return true;
     }
@@ -74,8 +77,7 @@ export default Ember.TextField.extend({
     if(!(isValGreater) && regex.test(this.get('value'))){
       newQty = this.get('item.quantity') - this.get('value');
       Ember.$(packagesLocationQtyId).text(newQty);
-    }
-    else{
+    } else {
       newQty = this.get('item.quantity');
       recordToSkipId = this.id;
       Ember.$(packagesLocationQtyId).text(newQty);
