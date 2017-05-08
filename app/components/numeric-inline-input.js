4d065962c4b2e246d08ead8e5acf3db919ebf9fa
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-05-08 13:50:33 +0530

    GCW-1540: cannot read property send of null

--- app/components/numeric-inline-input.js
+++ app/components/numeric-inline-input.js
@@ -1,13 +1,52 @@
 import Ember from 'ember';
-import NumericInput from './numeric-input';
 import AjaxPromise from 'stock/utils/ajax-promise';
 const { getOwner } = Ember;
 
-export default NumericInput.extend({
+export default Ember.TextField.extend({
   store: Ember.inject.service(),
   previousValue: '',
+  tagName: "input",
+  type: "text",
+  attributeBindings: [ "name", "type", "value", "maxlength", "id", "autoFocus" , "placeholder", "required", "pattern"],
+  classNameBindings: ["class"],
+
+  didInsertElement() {
+    if(this.attrs.autoFocus) { this.$().focus(); }
+  },
+
+  keyUp: function(){
+    var value = this.attrs.value.value;
+    var regexPattern = /^\d+$/;
+    if(value && value.toString().search(regexPattern) !== 0){
+      this.set('value', value.replace(/\D/g,''));
+    }
+    return true;
+  },
+
+  keyDown: function(e) {
+    var key = e.charCode || e.keyCode || 0;
+    this.set('currentKey', key);
+
+    // allow ctrl+v, enter, backspace, tab, delete, numbers, keypad numbers
+    // home, end only.
+    return (
+        (e.ctrlKey && key === 86) ||
+        key === 13 ||
+        key === 8 ||
+        key === 9 ||
+        key === 46 ||
+        key === 39 ||
+        (key >= 35 && key <= 37) ||
+        (key >= 48 && key <= 57) ||
+        (key >= 96 && key <= 105));
+  },
 
   focusOut() {
+    var val = this.attrs.value.value;
+    var regexPattern = /^\d+$/;
+    if(val && val.toString().search(regexPattern) !== 0){
+      this.set('value', val.replace(/\D/g,''));
+    }
     var item = this.get("item");
     var url = `/packages/${item.get('id')}`;
     var key = this.get('name');
