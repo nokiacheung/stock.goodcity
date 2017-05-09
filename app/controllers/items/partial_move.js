0cbffc9113da704df6c2a51872413a495e1ff739
trojanh <rajan.tiwari@kiprosh.com>
2017-05-09 18:57:41 +0530

    allow move for dispatch location packages

--- app/controllers/items/partial_move.js
+++ app/controllers/items/partial_move.js
@@ -12,6 +12,10 @@ export default Ember.Controller.extend({
     move_partial_qty(item) {
       var totalQty = 0;
       var allPackagesLocations = item.get('packagesLocationsList');
+      if(item.get('hasSingleAndDispatchLocation')){
+        allPackagesLocations = item.get('firstAllLocationName');
+      }
+
       var elementIds  = allPackagesLocations.getEach('id');
       var packagesLocationQty = [];
       var record = {};
