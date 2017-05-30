ac9d0d1bdb5a277015f0136395b254a14a079443
trojanh <rajan.tiwari@kiprosh.com>
2017-05-30 10:48:57 +0530

    GCW-1574 remove the cached image for new item creation

--- app/routes/items/new.js
+++ app/routes/items/new.js
@@ -58,18 +58,7 @@ export default AuthorizeRoute.extend({
       controller.set('height', null);
       controller.set('selectedGrade', { name: "B", id: "B" });
       controller.set('selectedCondition', { name: "Used", id: "U" });
-
-      var imageKey = controller.get("imageKeys");
-      if(imageKey && imageKey.length) {
-        var image = this.get("store").peekAll("image").filterBy("cloudinaryId", imageKey).get("firstObject");
-        image = image || this.get("store").createRecord("image", {
-            cloudinaryId: imageKey,
-            favourite: true
-          });
-        controller.set("newUploadedImage", image);
-      } else {
-        controller.set("newUploadedImage", null);
-      }
+      controller.set("newUploadedImage", null);
     }
   }
 
