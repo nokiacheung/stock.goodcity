e975edf27c997cd480eb2c5e5d835cbcae5d0a72
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-06-05 17:22:46 +0530

    GCW-1532 address and contact removed

--- app/controllers/subscription.js
+++ app/controllers/subscription.js
@@ -12,7 +12,7 @@ export default Ember.Controller.extend({
   lastOnline: Date.now(),
   deviceTtl: 0,
   deviceId: Math.random().toString().substring(2),
-  modelDataTypes: ["offer", "Offer", "item", "Item", "Schedule", "schedule", "delivery", "Delivery", "message", "Message", "gogovan_order", "address", "contact", "GogovanOrder", "Address", "Contact"],
+  modelDataTypes: ["offer", "Offer", "item", "Item", "Schedule", "schedule", "delivery", "Delivery", "message", "Message", "gogovan_order", "GogovanOrder"],
   // logger: Ember.inject.service(),
   status: {
     online: false
