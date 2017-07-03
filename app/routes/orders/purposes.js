c0ca1e2fcb7c6fe1233ef55b937475fc62ddf3cc
trojanh <rajan.tiwari@kiprosh.com>
2017-07-03 18:49:57 +0530

    Added purpose view and added GGV transport view

---
+++ app/routes/orders/purposes.js
@@ -0,0 +1,9 @@
+import getOrderRoute from './get_order';
+
+export default getOrderRoute.extend({
+  // model: function(){
+  //   // GET /items
+  //   // Retrieves all items.
+  //   return this.modelFor('designation').get('orderTransports');
+  // }
+});
