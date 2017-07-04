a9fef53ec51cf67b7490e5fbf54fb85ac5783e86
trojanh <rajan.tiwari@kiprosh.com>
2017-07-04 10:52:13 +0530

    removed comments

--- app/routes/orders/order_transport.js
+++ app/routes/orders/order_transport.js
@@ -1,9 +1,4 @@
 import getOrderRoute from './get_order';
 
 export default getOrderRoute.extend({
-  // model: function(){
-  //   // GET /items
-  //   // Retrieves all items.
-  //   return this.modelFor('designation').get('orderTransports');
-  // }
 });
