1638017abc683c28c3950b32fc8fe477e2af9b65
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-08-03 12:13:57 +0530

    Controller, route and component refactored

--- app/components/numeric-inline-input.js
+++ app/components/numeric-inline-input.js
@@ -23,22 +23,26 @@ export default Ember.TextField.extend({
     return true;
   },
 
+  whichKey(e, key) {
+    return ((e.ctrlKey && key === 86) ||
+      key === 13 ||
+      key === 8 ||
+      key === 9 ||
+      key === 46 ||
+      key === 39 ||
+      (key >= 35 && key <= 37) ||
+      (key >= 48 && key <= 57) ||
+      (key >= 96 && key <= 105));
+  },
+
   keyDown: function(e) {
     var key = e.charCode || e.keyCode || 0;
     this.set('currentKey', key);
 
     // allow ctrl+v, enter, backspace, tab, delete, numbers, keypad numbers
     // home, end only.
-    return (
-        (e.ctrlKey && key === 86) ||
-        key === 13 ||
-        key === 8 ||
-        key === 9 ||
-        key === 46 ||
-        key === 39 ||
-        (key >= 35 && key <= 37) ||
-        (key >= 48 && key <= 57) ||
-        (key >= 96 && key <= 105));
+    var keyPressed = this.whichKey(e, key);
+    return keyPressed;
   },
 
   focusOut() {
