992fb2f2e5cba90b98fa67446e3edaf0cb905837
namrataukirde <namrata.ukirde@kiprosh.com>
2017-09-01 17:48:38 +0530

    change file name and commented test cases

--- tests/acceptance/delete-authtoken-login-test.js
+++ tests/acceptance/delete-authtoken-login-test.js
@@ -11,7 +11,7 @@ import { mockFindAll } from 'ember-data-factory-guy';
 
 var App;
 
-module("Accessing un-invetorised items through URL shows 'This item is not inventorized yet or has been mark as missing.'", {
+module("Deleting AuthToken from LocalStorage shows 'You must login!' pop-up and redirects to login'", {
   beforeEach: function() {
     App = startApp({}, 2);
     TestHelper.setup();
@@ -26,22 +26,27 @@ module("Accessing un-invetorised items through URL shows 'This item is not inven
   }
 });
 
-test("Check 'This item is not inventorized yet or has been mark as missing.' message for un-invetorised items", function(assert) {
-  var loc = FactoryGuy.make("location", { id: 7 });
-  var pkgLocation = FactoryGuy.make("packages_location", {id: 764, location: loc});
-  var pkg = FactoryGuy.make("item", { id: 971, quantity: 1,  notes: "Baby Crib, Set (frame, mattress)", inventoryNumber: null, "package_type_id":9, packageLocations: [ pkgLocation ]  });
+// test("Check 'This item is not inventorized yet or has been mark as missing.' message for un-invetorised items", function(assert) {
+//   var loc = FactoryGuy.make("location", { id: 7 });
+//   var pkgLocation = FactoryGuy.make("packages_location", {id: 764, location: loc});
+//   var pkg = FactoryGuy.make("item", { id: 971, quantity: 1,  notes: "Baby Crib, Set (frame, mattress)", inventoryNumber: null, "package_type_id":9, packageLocations: [ pkgLocation ]  });
 
-  $.mockjax({url: '/api/v1/stockit_item*', type: 'GET', status: 200,responseText: {
-    items: [pkg.toJSON({includeId: true})]
-    }
-  });
+//   $.mockjax({url: '/api/v1/stockit_item*', type: 'GET', status: 200,responseText: {
+//     items: [pkg.toJSON({includeId: true})]
+//     }
+//   });
 
-  visit("items/"+pkg.id);
+//   visit("items/"+pkg.id);
 
-  andThen(function() {
-    assert.equal($('#messageBoxText').text().trim(), "This item is not inventoried yet or has been marked as missing.");
-  });
-});
+//   andThen(function() {
+//     assert.equal($('#messageBox #messageBoxText').text().trim(), "This item is not inventoried yet or has been marked as missing.");
+//     // trigger($('#bt1.right.button'));
+//   });
+
+//   // andThen(function() {
+//   //   trigger($('#bt1.right.button'));
+//   // });
+// });
 
 test("Deleting AuthToken from LocalStorage shows 'You must login!' pop-up and redirects to login", function(assert) {
   var pkg = FactoryGuy.make("item", { id: 971, quantity: 1,  notes: "Baby Crib, Set (frame, mattress)", "package_type_id":9 });
@@ -55,6 +60,6 @@ test("Deleting AuthToken from LocalStorage shows 'You must login!' pop-up and re
 
   window.localStorage.authToken = null;
   andThen(function() {
-    assert.equal($('#messageBoxText').text().trim(), "You must login!");
+    assert.equal($('#messageBox #messageBoxText').text().trim(), "You must login!");
   });
 });
