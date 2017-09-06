233bdd96dbae03658a28f75a2b2ffcb3348aa6f5
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-09-06 10:40:04 +0530

    OrdersPackage model test cases added

--- tests/unit/model/orders-package-test.js
+++ tests/unit/model/orders-package-test.js
@@ -5,7 +5,26 @@ moduleForModel('orders_package', 'OrdersPackage Model', {
   needs: ['model:item', 'model:image', 'model:donor_condition', 'model:user', 'model:designation', 'model:code']
 });
 
-test('OrdersPackage is a valid ember-data Model', function (assert) {
+test('Relationships with other models', function(assert) {
+  assert.expect(4);
+  var OrdersPackage = this.store().modelFor('orders_package');
+  var relationshipWithItem = Ember.get(OrdersPackage, 'relationshipsByName').get('item');
+  var relationshipWithDesignation = Ember.get(OrdersPackage, 'relationshipsByName').get('designation');
+
+  assert.equal(relationshipWithItem.key, 'item');
+  assert.equal(relationshipWithItem.kind, 'belongsTo');
+  assert.equal(relationshipWithDesignation.key, 'designation');
+  assert.equal(relationshipWithDesignation.kind, 'belongsTo');
+});
+
+test('Checking computed properties', function(assert) {
+  assert.expect(2);
+  var orders_package = this.subject({ state: "designated", quantity: 6 });
+  assert.equal(orders_package.get('availableQty'), 6);
+  assert.equal(orders_package.get('isSingleQuantity'), false);
+});
+
+test('OrdersPackage is a valid ember-data Model', function(assert) {
   assert.expect(1);
 
   var store  = this.store();
