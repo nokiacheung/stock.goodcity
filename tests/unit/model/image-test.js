e0d9a1881133fb23d336ebbb49ee6ed17976700e
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-09-07 12:27:49 +0530

    Image model test cases added

---
+++ tests/unit/model/image-test.js
@@ -0,0 +1,15 @@
+import { test, moduleForModel } from 'ember-qunit';
+import Ember from 'ember';
+
+moduleForModel('image', 'Image Model', {
+  needs: ['model:item']
+});
+
+test('Relationships with other models', function(assert) {
+  assert.expect(2);
+  var Image = this.store().modelFor('image');
+  var relationshipWithItem = Ember.get(Location, 'relationshipsByName').get('item');
+
+  assert.equal(relationshipWithItem.key, 'item');
+  assert.equal(relationshipWithItem.kind, 'belongsTo');
+});
