1cf1de3728094d1ea83d3a13db6a429be895362f
viveksriram1 <vivek.sriram@kiprosh.com>
2017-09-14 16:29:06 +0530

    Add test cases for code model

--- tests/unit/model/address-test.js
+++ tests/unit/model/address-test.js
@@ -6,23 +6,26 @@ moduleForModel('address', 'Address model',{
 });
 
 test('check attributes', function(assert){
+  assert.expect(5);
   var model = this.subject();
   var flat = Object.keys(model.toJSON()).indexOf('flat') > -1;
   var building = Object.keys(model.toJSON()).indexOf('building') > -1;
   var street = Object.keys(model.toJSON()).indexOf('street') > -1;
   var addressType = Object.keys(model.toJSON()).indexOf('addressType') > -1;
+  var addressableType = Object.keys(model.toJSON()).indexOf('addressableType') > -1;
 
   assert.ok(flat);
   assert.ok(building);
   assert.ok(street);
   assert.ok(addressType);
+  assert.ok(addressableType);
 });
 
 test('Relationships with other models', function(assert){
   assert.expect(4);
   var Address = this.store().modelFor('Address');
-  var relationshipWithAddressable =Ember.get(Address, 'relationshipsByName').get('addressable');
-  var relationshipWithDistrict =Ember.get(Address, 'relationshipsByName').get('district');
+  var relationshipWithAddressable = Ember.get(Address, 'relationshipsByName').get('addressable');
+  var relationshipWithDistrict = Ember.get(Address, 'relationshipsByName').get('district');
 
   assert.equal(relationshipWithAddressable.key, 'addressable');
   assert.equal(relationshipWithAddressable.kind, 'belongsTo');
