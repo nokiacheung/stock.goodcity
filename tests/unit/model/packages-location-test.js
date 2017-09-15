7c01a7b3648fb0e51e1ad5a10f532a599436b5cc
viveksriram1 <vivek.sriram@kiprosh.com>
2017-09-15 13:32:08 +0530

    add attribute testcases in package_location model

--- tests/unit/model/packages-location-test.js
+++ tests/unit/model/packages-location-test.js
@@ -5,6 +5,21 @@ moduleForModel('packages_location', 'PackagesLocation Model', {
   needs: ['model:item', 'model:image', 'model:donor_condition', 'model:user', 'model:designation', 'model:code', 'model:location']
 });
 
+
+test('check attributes', function(assert){
+  assert.expect(4);
+  var model = this.subject();
+  var packageId = Object.keys(model.toJSON()).indexOf('packageId') > -1;
+  var itemId = Object.keys(model.toJSON()).indexOf('itemId') > -1;
+  var quantity = Object.keys(model.toJSON()).indexOf('quantity') > -1;
+  var locationId = Object.keys(model.toJSON()).indexOf('locationId') > -1;
+
+  assert.ok(packageId);
+  assert.ok(itemId);
+  assert.ok(quantity);
+  assert.ok(locationId);
+});
+
 test('Relationships with other models', function(assert) {
   assert.expect(4);
   var PackagesLocation = this.store().modelFor('packages_location');
