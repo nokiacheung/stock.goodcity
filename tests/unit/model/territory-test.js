7bba062282c7b01935e4f913f17ea20e93ffb471
viveksriram1 <vivek.sriram@kiprosh.com>
2017-09-18 11:42:48 +0530

    add territory attribute test cases

--- tests/unit/model/territory-test.js
+++ tests/unit/model/territory-test.js
@@ -5,6 +5,14 @@ moduleForModel('Territory', 'Territory Model', {
   needs: ['model:territory', 'model:district']
 });
 
+test('check attributes', function(assert){
+  assert.expect(1);
+  var model = this.subject();
+  var name = Object.keys(model.toJSON()).indexOf('name') > -1;
+
+  assert.ok(name);
+});
+
 test('Relationships with other models', function(assert) {
   assert.expect(2);
   var Territory = this.store().modelFor('territory');
