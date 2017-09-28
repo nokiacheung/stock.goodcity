863f3fc617455ffdb0b416e84d9edba7eb62afc5
viveksriram1 <vivek.sriram@kiprosh.com>
2017-09-28 11:10:51 +0530

    add set_item model computed property test cases

--- tests/unit/model/set_item-test.js
+++ tests/unit/model/set_item-test.js
@@ -2,7 +2,7 @@ import { test, moduleForModel } from 'ember-qunit';
 import Ember from 'ember';
 
 moduleForModel('set_item', 'SetItem model',{
-  needs: ['model:code','model:item']
+  needs: ['model:code', 'model:item', 'model:designation', 'model:location', 'model:code', 'model:donor_condition', 'model:packages_location', 'model:orders_package', 'model:image', 'model:contact', 'model:organisation', 'model:local-order']
 });
 
 test('check attributes', function(assert){
@@ -23,4 +23,59 @@ test('Relationships with other models', function(assert) {
   assert.equal(relationshipWithCode.kind, 'belongsTo');
   assert.equal(relationshipWithItem.key, 'items');
   assert.equal(relationshipWithItem.kind, 'hasMany');
+});
+
+
+test('check multiQuantitySet computed property', function(assert){
+  var model, store, item1, item2, item3;
+  model = this.subject();
+  store = this.store();
+
+  Ember.run(function(){
+    item1 = store.createRecord('item', { id: 1, quantity: 2 });
+    item2 = store.createRecord('item', { id: 2, quantity: 3 });
+    item3 = store.createRecord('item', { id: 3, quantity: 1 });
+
+    model.get('items').pushObjects([item1, item2, item3]);
+  });
+
+  assert.equal(model.get('multiQuantitySet'), true);
+});
+
+test('check allDesignated copmuted property', function(assert){
+  var model, store, item1, item2, item3, designation;
+  model = this.subject();
+  store = this.store();
+
+  Ember.run(function(){
+    designation = store.createRecord('designation', {
+                id:               5,
+                detailType:       'StockitLocalOrder',
+                status:           'Active',
+                createdAt:        '12/07/2016',
+                updatedAt:        '12/07/2016'
+              });
+    item1 = store.createRecord('item', { id: 1, designation: designation });
+    item2 = store.createRecord('item', { id: 2, designation: designation });
+    item3 = store.createRecord('item', { id: 3, designation: null });
+
+    model.get('items').pushObjects([item1, item2, item3]);
+  });
+
+  assert.equal(model.get('allDesignated'), true);
+});
+
+test('check sortedItems computed property', function(assert){
+  var model, store, item1, item2, item3;
+  model = this.subject();
+  store = this.store();
+
+  Ember.run(function(){
+    item1 = store.createRecord('item', { id: 1, quantity: 1 });
+    item2 = store.createRecord('item', { id: 2, quantity: 1 });
+    item3 = store.createRecord('item', { id: 3, quantity: 1 });
+    model.get('items').pushObjects([item1, item2, item3]);
+  });
+
+  assert.equal(Ember.compare(model.get('sortedItems').getEach('id'), ["1", "2", "3"]), 0);
 });
