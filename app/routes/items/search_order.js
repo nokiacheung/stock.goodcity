d673e5e5ee5651b0e0495ffac7561f42da7b4fbe
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-10-10 16:41:16 +0530

    CI build fixed

--- app/routes/items/search_order.js
+++ app/routes/items/search_order.js
@@ -61,7 +61,7 @@ export default AuthorizeRoute.extend({
   },
 
   afterModel(model) {
-    if(model.get('quantity') === 0) {
+    if(model.item.get('quantity') === 0) {
       this.get('transition').abort();
       this.get("messageBox").alert("This item is already designated", () => {
         this.transitionTo("items.index");
