3fd913ce3e6919d3477c8389062ff0b024c71dcf
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2017-10-31 09:59:18 +0530

    Fingerprinting removed for sourcemap

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -9,8 +9,13 @@ module.exports = function(defaults) {
       enabled: true,
       extensions: ['js']
     },
+    minifyJS: {
+      options: {
+        exclude: ["**/vendor.js"]
+      }
+    },
     fingerprint: {
-      extensions: ['js', 'css', 'png', 'jpg', 'gif', 'map'],
+      extensions: ['js', 'css', 'png', 'jpg', 'gif'],
       enabled: webRelease
     },
     gzip: {
