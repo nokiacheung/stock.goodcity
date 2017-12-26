0cdf883dc3ab430d631943c164a2763c06d2721d
namrataukirde <namrata.ukirde@kiprosh.com>
2017-12-26 13:26:13 +0530

    separation of css

--- app/controllers/organisations/detail.js
+++ app/controllers/organisations/detail.js
@@ -1,5 +1,5 @@
 import Ember from "ember";
 
 export default Ember.Controller.extend({
-  filteredResults: Ember.computed.alias('model.orders')
+  backLinkPath: "",
 });
--- app/controllers/organisations/users.js
+++ app/controllers/organisations/users.js
@@ -1,6 +1,4 @@
-import config from '../../config/environment';
 import Ember from "ember";
-import searchModule from "../search_module";
 
 export default Ember.Controller.extend({
   allOrganisationsUsers: Ember.computed(function(){
