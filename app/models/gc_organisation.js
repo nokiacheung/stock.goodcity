da7cbb18477640e46e5a4d32c8f286225ef44264
namrataukirde <namrata.ukirde@kiprosh.com>
2018-01-03 19:43:57 +0530

    make all fields compulsary on new users form

--- app/models/gc_organisation.js
+++ app/models/gc_organisation.js
@@ -5,6 +5,9 @@ import Ember from "ember";
 import { hasMany } from 'ember-data/relationships';
 export default Model.extend({
   nameEn: attr('string'),
+  nameZhTw: attr('string'),
+  descriptionEn: attr('string'),
+  descriptionZhTw: attr('string'),
   website: attr('string'),
   registration: attr('string'),
 
