d93904edbaf3b76aa1d3c25bca641fa913588372
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-01-04 15:20:21 +0530

    GCW-1775 improvement

--- app/components/toggle-item-options-list.js
+++ app/components/toggle-item-options-list.js
@@ -1,14 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  item: null,
 
   click() {
-    var item = this.get("item");
-    var itemOptionsLink = Ember.$('.options-link-open.hidden.' + item.id)[0];
-    if(itemOptionsLink) {
-      Ember.$('.receive-item-options.' + item.id).toggleClass("hidden");
-      Ember.$('.options-link-open.' + item.id).toggleClass("hidden");
+    var itemOptionsLink = Ember.$('.options-link-open.hidden');
+    if(itemOptionsLink.length) {
+      Ember.$('.receive-item-options').not('.hidden').toggleClass('hidden');
+      Ember.$('.options-link-open.hidden').toggleClass('hidden');
       return false;
     } else {
       return true;
