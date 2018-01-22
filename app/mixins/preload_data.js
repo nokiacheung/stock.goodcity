f71c969860eb6347d3b6d3e5c52dad9fe53d68f5
namrataukirde <namrata.ukirde@kiprosh.com>
2018-01-22 20:53:43 +0530

    dont redirect to login if user is already logged in

--- app/mixins/preload_data.js
+++ app/mixins/preload_data.js
@@ -3,15 +3,18 @@ import AjaxPromise from '../utils/ajax-promise';
 
 export default Ember.Mixin.create({
   preloadData: function() {
+    var promises = [];
 
     if (this.get("session.authToken")) {
-      new AjaxPromise("/auth/current_user_profile", "GET", this.session.get("authToken"))
-        .then(data => {
-          this.store.pushPayload(data);
-          this.store.pushPayload({ user: data.user_profile });
-          this.notifyPropertyChange("session.currentUser");
-        }
+      promises.push(
+        new AjaxPromise("/auth/current_user_profile", "GET", this.session.get("authToken"))
+          .then(data => {
+            this.store.pushPayload(data);
+            this.store.pushPayload({ user: data.user_profile });
+            this.notifyPropertyChange("session.currentUser");
+          })
       );
     }
+    return Ember.RSVP.all(promises);
   }
 });
--- app/routes/session.js
+++ app/routes/session.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
+const { getOwner } = Ember;
 
 export default Ember.Route.extend({
 
   beforeModel() {
     if (this.session.get('isLoggedIn') && this.get('session.currentUser')) {
       this.transitionTo('/');
+    } else if(this.session.get('isLoggedIn')){
+      getOwner(this).lookup("route:application")._loadDataStore();
     } else {
       this.transitionTo('login');
     }
