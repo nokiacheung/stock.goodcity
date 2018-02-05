c9ed29452907e4cb3dc37d33fb3248cde97e6694
namrataukirde <namrata.ukirde@kiprosh.com>
2018-02-05 16:27:43 +0530

    Revert "add test cases removed accidentally"
    This reverts commit 0a054262b9484a4a88f787b23d98cbe973a463a2.

--- tests/controller/item-details-test.js
+++ tests/controller/item-details-test.js
@@ -1,22 +1,7 @@
 import { test, moduleFor } from 'ember-qunit';
-import startApp from '../helpers/start-app';
-import TestHelper from 'ember-data-factory-guy/factory-guy-test-helper';
-import Ember from 'ember';
-
-var App;
 
 moduleFor('controller:items.detail', 'items.detail controller', {
-  needs: ['service:messageBox', 'service:i18n'],
-
-  beforeEach: function() {
-    App = startApp({}, 2);
-    TestHelper.setup();
-
-  },
-  afterEach: function() {
-    Ember.run(function() { TestHelper.teardown(); });
-    Ember.run(App, 'destroy');
-  }
+  needs: ['service:messageBox', 'service:i18n']
 });
 
 test('Checking for default set values', function(assert) {
@@ -32,18 +17,3 @@ test('Checking for default set values', function(assert) {
   assert.equal(controller.get('displayScanner'), false);
   // assert.equal(controller.get('designateFullSet'), false);
 });
-
-test('calling partialDesignateForSet action sets designateFullSet and callOrderObserver property', function(assert) {
-  assert.expect(4);
-
-  var ctrl = this.subject();
-
-  assert.equal(ctrl.get('designateFullSet'), false);
-  assert.equal(ctrl.get('callOrderObserver'), false);
-
-  ctrl.send('partialDesignateForSet');
-
-  assert.equal(ctrl.get('designateFullSet'), true);
-  assert.equal(ctrl.get('callOrderObserver'), true);
-});
-
