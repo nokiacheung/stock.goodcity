ea602cd923a4348d4430d52ed602dc9a41b62093
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-02-07 10:39:33 +0530

    GCW-1748 all Organisation User loading issue fixed

--- app/controllers/organisations/users.js
+++ app/controllers/organisations/users.js
@@ -2,7 +2,5 @@ import Ember from "ember";
 
 export default Ember.Controller.extend({
   gcOrganisationUsersCount: null,
-  allOrganisationsUsers: Ember.computed(function(){
-    return this.store.peekAll("OrganisationsUser");
-  }),
+  allOrganisationsUsers: null
 });
--- app/routes/organisations/users.js
+++ app/routes/organisations/users.js
@@ -1,4 +1,8 @@
 import OrganisationRoute from './../organisation';
 
 export default OrganisationRoute.extend({
+  setupController(controller, model){
+    this._super(controller, model);
+    controller.set('allOrganisationsUsers', model.get('organisationsUsers'));
+  }
 });
