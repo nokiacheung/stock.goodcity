0202cb6abe0b83ea23a07b44d45ef1ba44be150b
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-02-12 11:34:03 +0530

    purpose test cases added

---
+++ tests/unit/model/purpose-test.js
@@ -0,0 +1,13 @@
+import { test, moduleForModel } from 'ember-qunit';
+import Ember from 'ember';
+
+moduleForModel('purpose', 'Purpose Model', {
+});
+
+test('check attributes', function(assert){
+  assert.expect(1);
+  var model = this.subject();
+  var nameEn = Object.keys(model.toJSON()).indexOf('nameEn') > -1;
+
+  assert.ok(nameEn);
+});
