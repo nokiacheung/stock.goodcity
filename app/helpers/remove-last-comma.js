39b9509d743d1f9071e2aadf4ea23dab080c2519
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-02-12 11:31:10 +0530

    Commneted code removed

--- app/helpers/remove-last-comma.js
+++ app/helpers/remove-last-comma.js
@@ -1,8 +1,5 @@
 import Ember from "ember";
 
-// Date Format used in App:
-// "12 Feb '16" => "DD MMM 'YY"
-
 export default Ember.Helper.helper(function(leftside) {
   return (leftside[1] + 1 < leftside[0]) ? true : false;
 });
