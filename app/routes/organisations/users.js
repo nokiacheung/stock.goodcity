2419fef9973517055dc8d599adf06fc90678e5e5
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-02-20 11:51:07 +0530

    New organisation user now showing on user list issue fixed

--- app/routes/organisations/users.js
+++ app/routes/organisations/users.js
@@ -3,6 +3,6 @@ import OrganisationRoute from './../organisation';
 export default OrganisationRoute.extend({
   setupController(controller, model){
     this._super(controller, model);
-    controller.set('allOrganisationsUsers', model.get('organisationsUsers'));
+    controller.set('allOrganisationsUsers', this.store.peekAll('organisationsUser').filterBy('organisationId', parseInt(model.id)));
   }
 });
