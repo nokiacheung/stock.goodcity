ef873aec5a774e42072964590133ab96d09c6d1d
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-03-08 11:06:30 +0530

    Address model test cases added

--- app/models/address.js
+++ app/models/address.js
@@ -1,6 +1,7 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { belongsTo } from 'ember-data/relationships';
+import Ember from 'ember';
 
 export default Model.extend({
   flat:        attr('string'),
@@ -11,5 +12,9 @@ export default Model.extend({
   district:    belongsTo('district', { async: false }),
 
   addressableType: attr('string'),
-  addressable: belongsTo('addressable', { polymorphic: true, async: false })
+  addressable: belongsTo('addressable', { polymorphic: true, async: false }),
+
+  fullAddress: Ember.computed('flat', 'building', 'street', function() {
+    return `${this.get('flat')} ${this.get('building')} ${this.get('street')}`;
+  })
 });
