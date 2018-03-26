4fe2b8a5e67eba09b86cd302c0f8ee564b1780d2
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-03-26 11:04:32 +0530

    GCW-1812- user profile data disappers

--- app/routes/orders/index.js
+++ app/routes/orders/index.js
@@ -1,3 +1,10 @@
 import AuthorizeRoute from './../authorize';
 
-export default AuthorizeRoute.extend();
+export default AuthorizeRoute.extend({
+  model() {
+    return new AjaxPromise("/auth/current_user_profile", "GET", this.session.get("authToken"))
+      .then(data => {
+        this.store.pushPayload(data);
+      });
+  }
+});
