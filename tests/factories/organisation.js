6aaf8c561afd99b8f1f57a840cfa9a858cd74c47
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-06-19 14:54:10 +0530

    Test cases added

--- tests/factories/organisation.js
+++ tests/factories/organisation.js
@@ -3,6 +3,7 @@ import FactoryGuy from 'ember-data-factory-guy';
 FactoryGuy.define('organisation',{
   default: {
     name:   FactoryGuy.generate('name'),
+    orders: FactoryGuy.hasMany('order')
   }
 });
 export default {};
