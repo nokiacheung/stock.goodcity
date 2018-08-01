b962a483f5b071b74ede2ab8b8c1902c8ac06f98
Darshan-Chauhan <darshan.chauhan@kiprosh.com>
2018-08-01 11:23:49 +0530

    Test cases fixed

--- tests/unit/model/contact-test.js
+++ tests/unit/model/contact-test.js
@@ -1,14 +1,15 @@
 import { test, moduleForModel } from 'ember-qunit';
+import Ember from 'ember';
 
-moduleForModel('contact', 'Contact model',{
-  need:[]
+moduleForModel('contact', 'Contact model', {
+  need:['model:addressable', 'model:address']
 });
 
-test('check attributes', function(assert){
-  var model = this.subject();
-  var mobile = Object.keys(model.toJSON()).indexOf('mobile') > -1;
-  var name = Object.keys(model.toJSON()).indexOf('name') > -1;
+test('Relationships with other models', function(assert){
+  assert.expect(2);
+  var contact = this.store().modelFor('contact');
+  var relationshipAddress = Ember.get(contact, 'relationshipsByName').get('address');
 
-  assert.ok(mobile);
-  assert.ok(name);
+  assert.equal(relationshipAddress.key, "address");
+  assert.equal(relationshipAddress.kind, "belongsTo");
 });
